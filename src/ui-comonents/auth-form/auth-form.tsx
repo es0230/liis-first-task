145eb372f08e3fc349c81617106a691e8a5fc2e8
moap <moap@liis.su>
2023-02-07 15:18:42 +0300

    fixed login button disabling

--- src/ui-comonents/auth-form/auth-form.tsx
+++ src/ui-comonents/auth-form/auth-form.tsx
@@ -1,5 +1,5 @@
 import { Formik } from "formik";
-import { ImageBackground, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { ImageBackground, Keyboard, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from "react-native";
 import * as yup from 'yup';
 
 import { AuthInputNames } from "../../constants/auth-input-names";
@@ -27,53 +27,55 @@ const AuthForm = () => {
         dispatch(logIn());
     };
 
-    return(
-        <View style={commonStyles.container}>
-            <ImageBackground style={styles.backgroundImage} resizeMode='cover' source={require('../../images/auth-background.png')}>
-                <View>
-                    <Text style={[commonStyles.text, { fontSize: 18 }]}>Добро пожаловать в</Text>
-                    <Text style={[commonStyles.text, commonStyles.screenHeader]}>Simple Hotel Check</Text>
-                </View>
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={commonStyles.container}>
+                <ImageBackground style={styles.backgroundImage} resizeMode='cover' source={require('../../images/auth-background.png')}>
+                    <View>
+                        <Text style={[commonStyles.text, { fontSize: 18 }]}>Добро пожаловать в</Text>
+                        <Text style={[commonStyles.text, commonStyles.screenHeader]}>Simple Hotel Check</Text>
+                    </View>
 
-                <Formik initialValues={formInitialValues} onSubmit={handleFormSubmit} validationSchema={validationSchema}>
-                    {({values, errors, touched, isValid, handleBlur, handleChange, handleSubmit}) => (
-                        <View style={{gap: 16}} >
-                            <Input
-                                placeholder="Логин"
-                                error={errors[AuthInputNames.Email]}
-                                touched={touched[AuthInputNames.Email]}
-                                onChangeText={handleChange(AuthInputNames.Email)}
-                                onBlur={handleBlur(AuthInputNames.Email)}
-                                value={values[AuthInputNames.Email]}
-                                name={AuthInputNames.Email}
-                            />
-                            
-                            <Input
-                                placeholder="Пароль"
-                                error={errors[AuthInputNames.Password]}
-                                touched={touched[AuthInputNames.Password]}
-                                onChangeText={handleChange(AuthInputNames.Password)}
-                                onBlur={handleBlur(AuthInputNames.Password)}
-                                value={values[AuthInputNames.Password]}
-                                name={AuthInputNames.Password}
-                            />
-                            
-                            <TouchableOpacity 
-                                onPress={handleSubmit} 
-                                activeOpacity={1} 
-                                style={[
-                                    commonStyles.submit,
-                                    (isValid && touched.email && touched.password) && commonStyles.submitEnabled,
-                                    { marginTop: 8 }
-                                ]}
-                            >
-                                <Text style={commonStyles.text}>Войти</Text>
-                            </TouchableOpacity>
-                        </View>                        
-                    )}
-                </Formik>
-            </ImageBackground>
-        </View>
+                    <Formik initialValues={formInitialValues} onSubmit={handleFormSubmit} validationSchema={validationSchema}>
+                        {({values, dirty, errors, touched, isValid, handleBlur, handleChange, handleSubmit}) => (
+                            <View style={{gap: 16}} >
+                                <Input
+                                    placeholder="Логин"
+                                    error={errors[AuthInputNames.Email]}
+                                    touched={touched[AuthInputNames.Email]}
+                                    onChangeText={handleChange(AuthInputNames.Email)}
+                                    onBlur={handleBlur(AuthInputNames.Email)}
+                                    value={values[AuthInputNames.Email]}
+                                    name={AuthInputNames.Email}
+                                />
+                                
+                                <Input
+                                    placeholder="Пароль"
+                                    error={errors[AuthInputNames.Password]}
+                                    touched={touched[AuthInputNames.Password]}
+                                    onChangeText={handleChange(AuthInputNames.Password)}
+                                    onBlur={handleBlur(AuthInputNames.Password)}
+                                    value={values[AuthInputNames.Password]}
+                                    name={AuthInputNames.Password}
+                                />
+                                
+                                <TouchableOpacity 
+                                    onPress={handleSubmit} 
+                                    activeOpacity={1} 
+                                    style={[
+                                        commonStyles.submit,
+                                        (isValid && dirty) && commonStyles.submitEnabled,
+                                        { marginTop: 8 }
+                                    ]}
+                                >
+                                    <Text style={commonStyles.text}>Войти</Text>
+                                </TouchableOpacity>
+                            </View>                        
+                        )}
+                    </Formik>
+                </ImageBackground>
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
